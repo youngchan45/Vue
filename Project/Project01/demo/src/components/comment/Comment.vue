ccbeb3509719d6bed68c1fbb820ec2107b6b7ba1
youngchan45 <youngchan93@163.com>
2019-10-21 17:51:41 +0800

    修改indexPage

--- Project/Project01/demo/src/components/comment/Comment.vue
+++ Project/Project01/demo/src/components/comment/Comment.vue
@@ -2,14 +2,14 @@
   <div>
     <div class="commentTitle">发表评论</div>
     <van-cell-group>
-      <van-field v-model="message" type="textarea" placeholder="请输入评论" rows="3" maxlength='10'/>
+      <van-field v-model="message" type="textarea" placeholder="请输入评论" rows="3" maxlength="10" />
     </van-cell-group>
     <van-button block type="info" @click="addComment">提交评论</van-button>
     <div>
       <!-- 一次请求：使用slice限制展现从0-a的数据，这样展示的话是不理睬后台的返回数据的分页 -->
-      <div v-for="(comment,index) in commentList.slice(0,a)" :key="index">
-          <!-- 分步请求：这里不用做限制，根据后台的数据分页返回即可 -->
-          <!-- <div v-for="(comment,index) in commentList" :key="index"> -->
+      <!-- <div v-for="(comment,index) in commentList.slice(0,a)" :key="index"> -->
+      <!-- 分步请求：这里不用做限制，根据后台的数据分页返回即可 -->
+      <div v-for="(comment,index) in commentList" :key="index">
         <div class="commentHead">
           <span>第{{index+1}}楼</span>
           <span>用户：{{comment.user_name}}</span>
@@ -66,12 +66,9 @@ export default {
       message: "",
       commentList: [],
       pageindex: 1,
-    //   currentPage:'',
+      //   currentPage:'',
       a: 5
     };
-  },
-  mounted(){
-    
   },
   //   易错：props和data同级！不是写在data里面
   props: ["id"],
@@ -80,6 +77,7 @@ export default {
     this.getComment();
   },
   methods: {
+    //加载第一页评论
     getComment() {
       axios
         .get(
@@ -89,42 +87,47 @@ export default {
             this.pageindex
         )
         .then(res => {
-        //   this.commentList = res.data.message;
-        //   旧列表拼接新列表
+          //   this.commentList = res.data.message;
+          //   旧列表拼接新列表
           this.commentList = this.commentList.concat(res.data.message);
           // console.log(res);
         });
     },
+    //点击加载更多按钮时
     getMore() {
-        //   使用实例中的东西记得加this
-    //   this.pageindex++;
-    //   this.currentPage=this.pageIndex;
-    this.a+=5;
-      this.getComment();     
+      //   使用实例中的东西记得加this
+      //页码+1
+      this.pageindex++;
+      //   this.currentPage=this.pageIndex;
+      // this.a += 5;
+      this.getComment();
     },
-addComment(){
-  axios.post('http://www.liulongbin.top:3005/api/postcomment/'+this.id).then(
-    res=>{
-      // console.log(res)
-
-      // toast的使用方法和官方文档不一样
-      // this.$toast({
-      //     message:'我是提示文字',
-      //   })
-//高级配置
-
-      const toast = this.$toast({
-  duration: 1000,  
-  forbidClick: true, // 禁用背景点击
-  message: res.data.message
-
-  
-});
+    addComment() {
+      axios
+      //易错点：这里的id是路由传过来的，应该是$route.params.id 而不是从data获取的
       
-      }
-  )
-}
+        .post("http://www.liulongbin.top:3005/api/postcomment/" + $route.params.id)
+        .then(res => {
+// res.data.commentList.unshift(this.message)
+// artid:this.id;
+// content:this.message;
+// url:'/api/postcomment/'+this.id;
+// res.data.content:this.message;
 
+if(res.data.status===0){
+  // toast的使用方法和官方文档不一样
+          // this.$toast({
+          //     message:'我是提示文字',
+          //   })
+          //高级配置
+const toast = this.$toast({
+            duration: 1000,
+            forbidClick: true, // 禁用背景点击
+            message: res.data.message
+          });
+}        
+        });
+    }
   }
 };
 </script>
